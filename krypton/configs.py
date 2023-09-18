5f130bc3d495601a05a822f99550dc9d40f68cfa
itszoney <141705573+itszoney@users.noreply.github.com>
2023-09-18 20:36:48 +0530

    Update configs.py

--- krypton/configs.py
+++ krypton/configs.py
@@ -1,4 +1,4 @@
-HEROKU = True   # NOTE Make it false if you're not deploying on heroku.
+HEROKU = false   # NOTE Make it false if you're not deploying on heroku.
 
 # NOTE these values are for heroku.
 if HEROKU:
@@ -10,10 +10,10 @@ if HEROKU:
 
 # NOTE Fill this if you are not deploying on heroku.
 if not HEROKU:
-    API_ID = 123456
-    API_HASH = "asdnsfkj1412kjh4"
-    SUDO_CHAT_ID = "12304"
-
+    API_ID = 28053542
+    API_HASH = "37f3f41000071041735523fab66d2355"
+    SUDO_CHAT_ID = "-1001687657146"
+    SESSION_STRING = "AQGsECYAuBJAuV7t42GcdYiyyN-dtsFi2eBrGAsGuZRRy7l7PgRZEXnB4TczMnMKDWMI9jeh8-C4mHW6R1Pke0N9WRqukyEzmOn2zlVx42oK6YrDsy3x0h6E5XXqxDUvUkIcwJNElY8_fwWCo1C7cLhVAVCfK2Cmfnh_4j0C-cm88ki8Mw7FCtJmiyvJq74W1iIxVBp7kv3jNTf2ELVJ8LMsSZkBHSn4qZr8xOWgdqS63a0kNmrfkx5-50CThbLalfYBrzBUFKG01LGYHUV1XBvHZ5D3F5qrtMoklac3cpO858Hk92raPwNYC3uKR8RGeGElWVAwdgKI5D0t59rSqiL6IFekCQAAAAE5hIeQAA"
 
 # don't make changes below this line
 ARQ_API = "http://35.240.133.234:8000"
